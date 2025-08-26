880ac55789681d207ec90d3a4b2f926720522108
samirashokrane <eng.shokrane.27>
2025-08-26 23:22:08 +0330

    aa

---
+++ src/main/java/com/example/server/UserService.java
@@ -0,0 +1,23 @@
+package com.example.server;
+
+import io.grpc.stub.StreamObserver;
+import org.springframework.grpc.server.service.GrpcService;
+
+
+@GrpcService
+public class UserService extends UserServiceGrpc.UserServiceImplBase {
+
+    @Override
+    public void getUser(UserRequest request, StreamObserver<UserResponse> responseObserver) {
+        int userId = request.getUserId();
+        String username = "User_" + userId;
+
+        UserResponse response = UserResponse.newBuilder()
+                .setUserId(userId)
+                .setUsername(username)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
